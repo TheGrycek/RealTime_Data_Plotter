00569eae35f37c9a51394170f47874e539f5321b
Mateusz Grycmacher <mateuszgrycmacher@gmail.com>
2021-09-08 23:40:52 +0200

    new sensors signals added, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RealTime_Data_Plotter
+
+Simple application for plotting real time accelerometer, gyroscope and encoders signals.
+
+<p align="center"> 
+<img src="./images/data_plotter.PNG">
+</p>
--- plotter_window.py
+++ plotter_window.py
@@ -1,4 +1,3 @@
-import random
 import re
 import sys
 from threading import Thread
@@ -8,15 +7,18 @@ import numpy as np
 import serial
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QTextEdit, \
-    QComboBox, QMainWindow, QGridLayout
+    QComboBox, QMainWindow, QGridLayout, QLabel
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
+
+from pathlib import Path
+import pandas as pd
 
 mpl.use('Qt5Agg')
 
 
 class Canvas(FigureCanvasQTAgg):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=5, height=8, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super(Canvas, self).__init__(fig)
@@ -29,28 +31,35 @@ class Window(QMainWindow):
         self.layout = QGridLayout(self)
         self.main_widget = QWidget(self)
         self.textbox = QTextEdit(self)
+        self.cursor = self.textbox.textCursor()
         self.comboBoxCOM = QComboBox(self)
         self.comboBoxBaud = QComboBox(self)
         self.comboBoxBit = QComboBox(self)
         self.comboBoxStopBits = QComboBox(self)
         self.comboBoxParity = QComboBox(self)
-        self.main_plot = Canvas(self, width=5, height=4, dpi=100)
+        self.comboBoxSensor = QComboBox(self)
+        self.main_plot = Canvas(self, width=5, height=7, dpi=100)
         self.toolbar = NavigationToolbar2QT(self.main_plot, self)
 
-        self.timebase = 100  # ms
-        self.n_data = 50
-        self.stop_flag = False
+        self.timebase = 50  # ms
+        self.n_data = 100
+        self.x_data_plot = list(range(self.n_data))
+        self.plot_yrange = {"acc": [-2200, 2200], "gyr": [-1500000, 1500000], "enc": [-50, 50]}
         self.x_data = np.array([])
-        self.y_data = np.array([])
-        self._plot_ref = None
+        self.y_data = [[np.array([]) for _ in range(3)] for _ in range(3)]  # accel, gyro, enc
+        self._plot_refs = [None, None, None]
+        self._stop_flag = False
+        self._reading_thread = None
+        self.plot_colours = ("r", "g", "b")
 
         self._title = "DATA PLOTTER"
-        self._positions = (200, 100, 1100, 600)  # right, down, width, height
+        self._positions = (200, 100, 1200, 900)  # right, down, width, height
         self.com_port = "COM3"
         self.baud_rate = "115200"
         self.data_bits = "8"
         self.parity = "NONE"
         self.stop_bits = "1"
+        self.sensor = "ACCELEROMETER"
 
         self.init_main_widget()
         self.setCentralWidget(self.main_widget)
@@ -65,6 +74,7 @@ class Window(QMainWindow):
     def init_basic_elements(self):
         self.setWindowTitle(self._title)
         self.setGeometry(*self._positions)
+        self.setFixedSize(*self._positions[2:])
 
         buttons = {"OPEN": {"pos": (10, 10, 100, 22), "func": self.start_reading},
                    "CLOSE": {"pos": (110, 10, 100, 22), "func": self.stop_reading},
@@ -79,21 +89,34 @@ class Window(QMainWindow):
         bits = [str(i) for i in range(8, 4, -1)]
         stop_bits = [str(i) for i in range(1, 3)]
         ports = [f"COM{i}" for i in range(1, 11)]
+        sensors = ["ACCELEROMETER", "GYROSCOPE", "ENCODERS"]
         parity = ('NONE', 'ODD', 'EVEN', 'MARK', 'SPACE')
         baud_rates = ("115200", "57600", "38400", "19200", "9600", "1200", "300", "921600", "460800",
                       "230400", "4800", "2400", "150", "110")
 
-        combo_boxes = {self.comboBoxCOM: {"items": ports, "func": self.combo_com_change, "pos": (500, 10)},
-                       self.comboBoxBaud: {"items": baud_rates, "func": self.combo_com_change, "pos": (610, 10)},
-                       self.comboBoxBit: {"items": bits, "func": self.combo_com_change, "pos": (720, 10)},
-                       self.comboBoxParity: {"items": parity, "func": self.combo_com_change, "pos": (830, 10)},
-                       self.comboBoxStopBits: {"items": stop_bits, "func": self.combo_com_change, "pos": (940, 10)}}
+        combo_boxes = {self.comboBoxCOM: {"items": ports, "func": self.combo_com_change, "pos": (500, 10),
+                                          "def": ports.index(self.com_port), "label": "Port"},
+                       self.comboBoxBaud: {"items": baud_rates, "func": self.combo_bit_change, "pos": (610, 10),
+                                           "def": baud_rates.index(self.baud_rate), "label": "Baud"},
+                       self.comboBoxBit: {"items": bits, "func": self.combo_bit_change, "pos": (720, 10),
+                                          "def": bits.index(self.data_bits), "label": "Data bits"},
+                       self.comboBoxParity: {"items": parity, "func": self.combo_parity_change, "pos": (830, 10),
+                                             "def": parity.index(self.parity), "label": "Parity"},
+                       self.comboBoxStopBits: {"items": stop_bits, "func": self.combo_stopbits_change, "pos": (940, 10),
+                                               "def": stop_bits.index(self.stop_bits), "label": "Stop bits"},
+                       self.comboBoxSensor: {"items": sensors, "func": self.combo_sensor_change, "pos": (1050, 10),
+                                             "def": sensors.index(self.sensor), "label": "Sensors"}}
 
         for combo_box, combo_params in combo_boxes.items():
             for item in combo_params["items"]:
                 combo_box.addItem(item)
             combo_box.move(*combo_params["pos"])
+            combo_box.setCurrentIndex(combo_params["def"])
             combo_box.currentIndexChanged.connect(combo_params["func"])
+
+            combo_label = QLabel(self)
+            combo_label.setText(combo_params["label"])
+            combo_label.move(combo_params["pos"][0], 35)
 
         self.show()
 
@@ -112,64 +135,90 @@ class Window(QMainWindow):
     def combo_stopbits_change(self):
         self.stop_bits = self.comboBoxStopBits.currentText()
 
+    def combo_sensor_change(self):
+        self.sensor = self.comboBoxSensor.currentText()
+        self.main_plot.axes.set_ylim(self.plot_yrange[self.sensor.lower()[:3]])
+
+    def set_cursor(self):
+        self.cursor.movePosition(QtGui.QTextCursor.End)
+        self.textbox.setTextCursor(self.cursor)
+
     def start_reading(self):
-        self.read_port()
+        self.main_plot.axes.set_ylim(self.plot_yrange[self.sensor.lower()[:3]])
+        if self._reading_thread is None:
+            self._stop_flag = False
+            self.read_port()
+        else:
+            self.textbox.append(f"Port already opened.\r")
+            self.set_cursor()
 
     def stop_reading(self):
-        self.stop_flag = True
+        if self._reading_thread is not None:
+            self._stop_flag = True
+            self._reading_thread.join()
+            self._reading_thread = None
         self.textbox.append(f"Port {self.com_port} closed.\n")
+        self.set_cursor()
 
     def clear_window(self):
         self.textbox.setText("")
+        self._plot_refs = [None, None, None]
         self.main_plot.axes.cla()
         self.main_plot.draw()
+        self.main_plot.axes.set_ylim(self.plot_yrange[self.sensor.lower()[:3]])
 
     def save_data(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getSaveFileName(self, "Save file", "c:\\",
-                                                   "All Files (*);;Text Files (*.txt)", options=options)
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save file", "c:\\", "CSV Files (*.csv)", options=options)
+
         if file_name:
-            print(file_name)
+            df = pd.DataFrame({"time": self.x_data,
+                               "accelX": self.y_data[0][0],
+                               "accelY": self.y_data[0][1],
+                               "accelZ": self.y_data[0][2],
+                               "gyroX": self.y_data[1][0],
+                               "gyroY": self.y_data[1][1],
+                               "gyroZ": self.y_data[1][2],
+                               "enc1": self.y_data[2][0],
+                               "enc2": self.y_data[2][1],
+                               "enc3": self.y_data[2][2]})
+
+            df.to_csv(Path(file_name), index=False)
 
     def plot_data(self):
-        if self.x_data.size >= 50:
-            y_data_plot = self.y_data[-50:]
-            x_data_plot = self.x_data[-50:]
-        else:
-            y_data_plot = self.y_data
-            x_data_plot = self.x_data
+        y_data = {"ACCELEROMETER": self.y_data[0], "GYROSCOPE": self.y_data[1], "ENCODERS": self.y_data[2]}
 
-        print(f"X: {x_data_plot}\nY: {y_data_plot}")
+        if y_data[self.sensor][0].size >= self.n_data:
+            y_data_plot = [data[-self.n_data:] for data in y_data[self.sensor]]
+            for i, colour in enumerate(self.plot_colours):
+                if self._plot_refs[i] is None:
+                    self._plot_refs[i] = self.main_plot.axes.plot(self.x_data_plot, y_data_plot[i], colour)[0]
+                else:
+                    self._plot_refs[i].set_ydata(y_data_plot[i])
 
-        if self._plot_ref is None:
-            self._plot_ref = self.main_plot.axes.plot(x_data_plot, y_data_plot, 'r')[0]
-        else:
-            self._plot_ref.set_xdata(x_data_plot)
-            self._plot_ref.set_ydata(y_data_plot)
-
-        self.main_plot.draw()
+            self.main_plot.draw()
 
     def reading_loop(self, serial):
-        while not self.stop_flag:
+        while not self._stop_flag:
             data = serial.readline()
             data = str(data.decode('utf-8'))
             self.textbox.append(data)
-            cursor = self.textbox.textCursor()
-            cursor.movePosition(QtGui.QTextCursor.End)
-            self.textbox.setTextCursor(cursor)
+            self.cursor.movePosition(QtGui.QTextCursor.End)
+            self.textbox.setTextCursor(self.cursor)
 
-            if data.startswith("Accel:"):
-                span = re.search(r'\[[^\]]*\]', data).span()
-                data_list = data[span[0] + 1:span[1] - 1].split(",")
+            data_list = data.split("; ")
 
-                if self.x_data.size > 0:
-                    self.y_data = np.append(self.y_data, int(data_list[0]))
-                    self.x_data = np.append(self.x_data, self.x_data[-1] + self.timebase)
-                    self.plot_data()
-                else:
-                    self.y_data = np.append(self.y_data, int(data_list[0]))
-                    self.x_data = np.append(self.x_data, self.timebase)
+            for sens, sensor_data in enumerate(data_list):
+                span = re.search(r'\[[^\]]*\]', sensor_data).span()
+                data_axes = sensor_data[span[0] + 1:span[1] - 1].split(",")
+                for ax in range(len(data_axes)):
+                    self.y_data[sens][ax] = np.append(self.y_data[sens][ax], int(data_axes[ax]))
+
+            self.x_data = np.append(self.x_data, (len(self.x_data) * self.timebase) + self.timebase)
+
+            if self.x_data.size > 0:
+                self.plot_data()
 
     def read_port(self):
         try:
@@ -179,11 +228,13 @@ class Window(QMainWindow):
                                 parity=self.parity[0],
                                 stopbits=int(self.stop_bits))
             self.textbox.append(f"Port {self.com_port} opened.\r")
-            reading_proc = Thread(target=self.reading_loop, args=(ser,), daemon=True)
-            reading_proc.start()
+            self.set_cursor()
+            self._reading_thread = Thread(target=self.reading_loop, args=(ser,), daemon=True)
+            self._reading_thread.start()
 
         except serial.serialutil.SerialException:
             self.textbox.append(f"No device found on {self.com_port}.\n")
+            self.set_cursor()
 
 
 if __name__ == "__main__":
